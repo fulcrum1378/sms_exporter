d32422fab21db744b1912bbd42abfd1e2aceaeac
Mahdi Parastesh <fulcrum1378@gmail.com>
2021-07-04 01:47:58 +0430

    2021.07.04 - 1

--- app/src/main/java/com/mahdiparastesh/smsexporter/data/PdfExporter.kt
+++ app/src/main/java/com/mahdiparastesh/smsexporter/data/PdfExporter.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.mahdiparastesh.smsexporter.Fun.Companion.c
 import com.mahdiparastesh.smsexporter.R
@@ -19,26 +21,39 @@ class PdfExporter(thread: SMS.Thread, contact: Contact?, where: Uri) :
 
     @SuppressLint("InflateParams")
     override fun run() { // DOES NOT INHERIT SUPER'S
-        val content = arrayListOf<ConstraintLayout>()
-        for (i in thread.list.indices) content.add(
-            (LayoutInflater.from(c)
-                .inflate(R.layout.item_sms, null, false) as ConstraintLayout).apply {
-                SmsAdap.prepare(thread.list, this, i)
-            })
         val document = PdfDocument()
 
         var page = 0
         var mess = 0
-        while (mess < content.size) {
-            document.startPage(
-                PdfDocument.PageInfo.Builder(size.width, size.height, page).create()// A4
-            ).apply {
-                canvas.scale(1f, 1f)
-                mess = insert(canvas, content, mess, size.height)
-                document.finishPage(this)
-            }
+        while (mess < thread.list.size) document.startPage(
+            PdfDocument.PageInfo.Builder(size.width, size.height, page).create()
+        ).apply {
+            canvas.scale(1f, 1f)
+            mess = insert(canvas, mess)
+            document.finishPage(this)
             page++
         }
+
+
+        /*val ll = LinearLayout(c).apply {
+            orientation = LinearLayout.VERTICAL
+            for (x in 0 until 10) addView(TextView(c).also { v ->
+                v.text = "Salam $x"
+            })
+        }
+        document.startPage(
+            PdfDocument.PageInfo.Builder(size.width, size.height, 1).create()
+        ).apply {
+            canvas.scale(1f, 1f)
+            ll.measure( // ESSENTIAL
+                View.MeasureSpec.makeMeasureSpec(canvas.width, View.MeasureSpec.AT_MOST),
+                View.MeasureSpec.makeMeasureSpec(canvas.height, View.MeasureSpec.AT_MOST)
+            )
+            throw IllegalAccessError("${ll.measuredHeight}")
+            ll.layout(0, 0, canvas.width, canvas.height)
+            ll.draw(canvas)
+            document.finishPage(this)
+        }*/
 
         c.contentResolver.openFileDescriptor(where, "w")?.use {
             FileOutputStream(it.fileDescriptor).use { fos ->
@@ -49,21 +64,26 @@ class PdfExporter(thread: SMS.Thread, contact: Contact?, where: Uri) :
         done(1)
     }
 
-    fun insert(canvas: Canvas, content: List<ConstraintLayout>, mess: Int, remnant: Int): Int {
+    fun insert(canvas: Canvas, mess: Int): Int {
         var iMess = mess
-        var iRemnant = remnant
-        content[iMess].apply {
-            measure( // ESSENTIAL
-                View.MeasureSpec.makeMeasureSpec(canvas.width, View.MeasureSpec.EXACTLY),
-                View.MeasureSpec.makeMeasureSpec(canvas.height, View.MeasureSpec.EXACTLY)
-            )
+        LinearLayout(c).apply {
+            orientation = LinearLayout.VERTICAL
+            do {
+                addView((LayoutInflater.from(c)
+                    .inflate(R.layout.item_sms, this, false) as ConstraintLayout).apply {
+                    SmsAdap.prepare(thread.list, this, iMess)
+                    // FIX THE VIEW
+                })
+                //for (x in 0 until 100) addView(TextView(c).apply { text = "Salam - $iMess" })
+                measure( // ESSENTIAL BOTH FOR draw() and measuredHeight
+                    View.MeasureSpec.makeMeasureSpec(canvas.width, View.MeasureSpec.AT_MOST),
+                    View.MeasureSpec.makeMeasureSpec(canvas.height, View.MeasureSpec.AT_MOST)
+                )
+                iMess++
+            } while (measuredHeight < size.height && iMess < thread.list.size)
             layout(0, 0, canvas.width, canvas.height)
             draw(canvas)
-            iRemnant -= this.measuredHeight
         }
-        iMess++
-        if (iMess < content.size && iRemnant > content[iMess].measuredHeight)
-            iMess = insert(canvas, content, iMess, iRemnant)
         return iMess
     }
 }
